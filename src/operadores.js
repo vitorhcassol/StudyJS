e3a214dd3ab7e6173f95aa22d494e7878d7974ba
Vitor Hugo Cassol <vhcassol@gmail.com>
2022-05-02 18:41:35 -0400

    Entendendo operadores de soma e subtração

---
+++ src/operadores.js
@@ -0,0 +1,16 @@
+//------------------Adição e Subtração------------
+
+//Adição
+let a = 0;
+console.log(a);
+a += 5;
+console.log(a);
+a++;
+console.log(a);
+
+//Subtração
+let b = 10;
+b -= 5;
+console.log(b);
+b--;
+console.log(b);
