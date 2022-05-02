684c64b4ffa43dcf1f096697d7883a28d56a890c
Vitor Hugo Cassol <vhcassol@gmail.com>
2022-05-02 19:30:30 -0400

    Entendendo multiplicação, divisão e módulo

--- src/operadores.js
+++ src/operadores.js
@@ -14,3 +14,24 @@ b -= 5;
 console.log(b);
 b--;
 console.log(b);
+
+//------------------Multiplicação, Divisão e Módulo------------
+
+//Multiplicação
+console.log(2 * 3);
+
+//Divisão
+console.log(10 / 5);
+
+//Módulo = resto da divisão
+console.log(2 % 2);
+
+const ParOuImpar = (n) => {
+    if (n % 2 === 0) {
+        console.log(`O número ${n} é par!`)
+    } else {
+        console.log(`O número ${n} é impar!`)
+    }
+}
+
+//ParOuImpar(2);
